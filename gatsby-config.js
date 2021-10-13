4d92c4d5ce7e3a6b2c75413552f09e1d0ead68c8
TerenceJDew <terence.dew@gmail.com>
2021-10-13 11:14:27 -0400

    Removed Data

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,15 +2,5 @@ module.exports = {
   siteMetadata: {
     siteUrl: "https://embed.ynhh.org",
     title: "Embed Links",
-  },
-  plugins: [
-    {
-      resolve: `gatsby-source-filesystem`,
-      options: {
-        name: `data`,
-        path: `${__dirname}/src/data/`,
-        ignore: [`**/\.*`], // ignore files starting with a dot
-      }
-    }
-  ],
+  }
 };
