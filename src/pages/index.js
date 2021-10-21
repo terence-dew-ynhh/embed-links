3df633b7bba2952d8fd24b962a553b7d5972be49
TerenceJDew <terence.dew@gmail.com>
2021-10-21 18:07:08 -0400

    Updated Styling

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,11 @@
 import * as React from "react";
 import EmbedLogo from "../images/embedlogo.jpg";
+import Emergency from "../images/emergencymed.png";
+import BUPinit from "../images/bup.png";
+import MDCalc from "../images/mdcalc.png";
+import Treatment from "../images/treatment.png";
+import NIHCollab from "../images/nihcollab.png";
+
 import Box from "@mui/material/Box";
 import Button from "@mui/material/Button";
 import Modal from "@mui/material/Modal";
@@ -30,7 +36,15 @@ const linkStyle = {
 };
 
 const img = {
-  height: 120,
+  height: 110,
+};
+
+const headerimg = {
+  height: 130,
+};
+
+const horizontalimg = {
+  width: 250,
 };
 
 // data
@@ -39,11 +53,15 @@ const links = [
     text: "EMBED web application (formatted for Epic integration) ",
     url: "https://embedv2.ynhh.org/",
     color: "#E95800",
+    header: "Where can you find EMBED?",
+    subheader:
+      "EMBED can be installed within the EHR or accessed via a smartphone app.",
   },
   {
     text: "EMBED BUP initiation iPhone app",
     url: "https://apps.apple.com/us/app/bup-initiation/id1574350314#?platform=iphone",
     color: "#1099A8",
+    img: BUPinit,
   },
   {
     text: "EMBED BUP initiation Android/Google smartphone app",
@@ -52,12 +70,13 @@ const links = [
   },
   {
     text: "EMBED on MDCalc (web version)",
-    url: "https://www.gatsbyjs.com/docs/conceptual/",
+    url: "https://nam12.safelinks.protection.outlook.com/?url=https%3A%2F%2Fwww.mdcalc.com%2Femergency-department-initiated-buprenorphine-opioid-use-disorder-embed&data=04%7C01%7Cterence.dew%40yale.edu%7Cec4a4484063d4b73271a08d994b7974d%7Cdd8cbebb21394df8b4114e3e87abeb5c%7C0%7C0%7C637704338502789632%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&sdata=mqyAJT45emw3rePt5ulltcleiT6xbC15ANYjDe%2F1U8M%3D&reserved=0",
     color: "#0D96F2",
+    img: MDCalc,
   },
   {
     text: "EMBED on MDCalc (app version) ",
-    url: "",
+    url: "https://apps.apple.com/us/app/mdcalc-medical-calculator/id1001640662",
     color: "#8EB814",
   },
   {
@@ -102,14 +121,16 @@ const links = [
       },
     ],
     color: "#663399",
+    header: "Where can you find EMBED?",
   },
   {
     text: "EMBED NIH Collaboratory Project Page ",
     url: "https://nam12.safelinks.protection.outlook.com/?url=https%3A%2F%2Frethinkingclinicaltrials.org%2Fdemonstration-projects%2Fug3-project-pragmatic-trial-of-user-centered-clinical-decision-support-to-implement-emergency-department-initiated-buprenorphine-for-opioid-use-disorder-embed%2F&data=04%7C01%7Cterence.dew%40yale.edu%7Ccb8daefb1f1f4a4e178708d9875d5db2%7Cdd8cbebb21394df8b4114e3e87abeb5c%7C0%7C0%7C637689657341588189%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&sdata=WuHQoDyTj6duirKSGqLnLIThvuME5Y4SQ23pcB0dsP8%3D&reserved=0",
     color: "#8EB814",
+    img: NIHCollab,
   },
   {
-    text: "EMBED Publications",
+    text: "EMBED Related Publications",
     url: "",
     sublinks: [
       {
@@ -151,6 +172,7 @@ const links = [
       },
     ],
     color: "#663399",
+    img: Treatment,
   },
   {
     text: "EMBED NIH RePORTER ",
@@ -158,19 +180,19 @@ const links = [
     color: "#BC027F",
   },
   {
-    text: "Clinicaltrials.gov",
+    text: "EMBED on Clinicaltrials.gov",
     url: "https://nam12.safelinks.protection.outlook.com/?url=https%3A%2F%2Fclinicaltrials.gov%2Fct2%2Fshow%2FNCT03658642&data=04%7C01%7Cterence.dew%40yale.edu%7Ccb8daefb1f1f4a4e178708d9875d5db2%7Cdd8cbebb21394df8b4114e3e87abeb5c%7C0%7C0%7C637689657341648154%7CUnknown%7CTWFpbGZsb3d8eyJWIjoiMC4wLjAwMDAiLCJQIjoiV2luMzIiLCJBTiI6Ik1haWwiLCJXVCI6Mn0%3D%7C1000&sdata=hhZ%2FBwmiG38BnMjmEaXpgKSl1HOAfEPCj538%2BOrxEPY%3D&reserved=0",
     color: "#0D96F2",
   },
 ];
 
 const modal = {
-  position: 'absolute',
-  top: '50%',
-  left: '50%',
-  transform: 'translate(-50%, -50%)',
+  position: "absolute",
+  top: "50%",
+  left: "50%",
+  transform: "translate(-50%, -50%)",
   width: 400,
-  bgcolor: 'background.paper',
+  bgcolor: "background.paper",
   boxShadow: 24,
   p: 4,
 };
@@ -184,15 +206,15 @@ const IndexPage = () => {
       <title>Home Page</title>
       <Modal
         open={open}
-        onClose={()=> setOpen(false)}
+        onClose={() => setOpen(false)}
         aria-labelledby="modal-modal-title"
         aria-describedby="modal-modal-description"
       >
         <Box sx={modal}>
           <Typography id="modal-modal-title" variant="h6" component="h2">
-            Send me more information about how I can have EMBED in my organization:
+            Send me more information about how I can have EMBED in my
+            organization:
           </Typography>
-          
         </Box>
       </Modal>
       <Box
@@ -203,45 +225,113 @@ const IndexPage = () => {
           padding: "0 3% 0 1%",
         }}
       >
-        <img style={img} src={EmbedLogo} />
-        <Button variant="contained" onClick={()=> setOpen(true)}>Contact us about using EMBED</Button>
+        <img style={headerimg} alt="topic logo" src={EmbedLogo} />
+        <h2>
+          EMBED: Tools & Resources for ED Initiation of Buprenorphine for Opioid
+          Use Disorder Patients
+        </h2>
+        <img style={headerimg} alt="topic logo" src={Emergency} />
+      </Box>
+      <Box>
+        <p>
+          <h3>What is EMBED?</h3>
+          EMBED is an electronic tool that can help clinicians initiate
+          buprenorphine in the ED for patients with opioid use disorder.
+        </p>
+        <p>
+          <h3>What can EMBED do?</h3>
+          EMBED guides clinicians in (1) determining a diagnosis of OUD, (2)
+          assessing withdrawal severity using COWS Scale, (3) Tools for
+          Motivational interviewing for patients not ready to accept treatment
+          and (4) determine treatment dose (5) help in automatic order entry,
+          populate clinician notes with visit summary and referral information.
+        </p>
+        <p>
+          <b> Demo video of EMBED</b>
+          <br></br>
+          <a
+            style={linkStyle}
+            target="_blank"
+            rel="noreferrer"
+            href={"https://www.youtube.com/watch?v=-XY4EIdaNKw"}
+          >
+            https://www.youtube.com/watch?v=-XY4EIdaNKw
+          </a>
+        </p>
       </Box>
       <ul style={listStyles}>
-        {links.map((link) => (
-          <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
-            <span>
-              <a
-                style={linkStyle}
-                target="_blank"
-                href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-              >
-                {link.text}
-              </a>
-              {/* <p style={descriptionStyle}>{link.description}</p> */}
-            </span>
-            <ul style={listStyles}>
-              {link.sublinks &&
-                link.sublinks.map((sublink) => (
-                  <li
-                    key={sublink.url}
-                    style={{ ...listItemStyles, color: sublink.color }}
+        {links.map((link, idx) => (
+          <>
+            {link.img && (
+              <img
+                style={idx === 6 || idx === 9 ? horizontalimg : img}
+                alt="topic logo"
+                src={link.img}
+              />
+            )}
+            {link.header && <h3>{link.header}</h3>}
+            {link.subheader && <h4>{link.subheader}</h4>}
+            <li key={link.url} style={{ ...listItemStyles, color: link.color }}>
+              <span>
+                {link.url !== "" ? (
+                  <a
+                    style={linkStyle}
+                    target="_blank"
+                    rel="noreferrer"
+                    href={`${link.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
                   >
-                    <span>
-                      <a
-                        style={linkStyle}
-                        target="_blank"
-                        href={`${sublink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
-                      >
-                        {sublink.text}
-                      </a>
-                      {/* <p style={descriptionStyle}>{link.description}</p> */}
-                    </span>
-                  </li>
-                ))}
-            </ul>
-          </li>
+                    {link.text}
+                  </a>
+                ) : (
+                  <p style={linkStyle}>{link.text}</p>
+                )}
+              </span>
+              <ul style={listStyles}>
+                {link.sublinks &&
+                  link.sublinks.map((sublink) => (
+                    <li
+                      key={sublink.url}
+                      style={{ ...listItemStyles, color: sublink.color }}
+                    >
+                      <span>
+                        <a
+                          style={linkStyle}
+                          target="_blank"
+                          rel="noreferrer"
+                          href={`${sublink.url}?utm_source=starter&utm_medium=start-page&utm_campaign=minimal-starter`}
+                        >
+                          {sublink.text}
+                        </a>
+                        {/* <p style={descriptionStyle}>{link.description}</p> */}
+                      </span>
+                    </li>
+                  ))}
+              </ul>
+            </li>
+          </>
         ))}
       </ul>
+      <ul>
+        <h2>Contact:</h2>
+        <li>
+          For EMBED IT Installation questions, contact: Terence Dew -{" "}
+          <a href="mailto:terence.dew@yale.edu">terence.dew@yale.edu</a>
+        </li>
+        <li>
+          For all other general questions, contact: Bidisha Nath Bidisha -{" "}
+          <a href="mailto:nath@yale.edu">nath@yale.edu</a>
+        </li>
+      </ul>
+      <h3>
+        Project PI:{" "}
+        <a
+          target="_blank"
+          rel="noreferrer"
+          href="https://medicine.yale.edu/profile/edward_melnick/"
+        >
+          Edward Melnick, MD, MHS{" "}
+        </a>
+      </h3>
     </main>
   );
 };
